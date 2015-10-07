23e63b116c4ac5a617fd05a4c4b6064469a69f08
Vandit18 <vanditkothari@gmail.com>
2015-10-07 15:14:07 -0400

    Assignment Commits

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'First Express Generator Site',
-                        message: 'Our first page'} );
+  res.render('index', { title: 'My Portfolio',
+                        message: ''} );
 });
 
 
-/* GET ABOUT page */
-router.get('/about', function(req, res, next) {
-   res.render('about'); 
+
+
+/* GET About page */
+router.get('/About', function(req, res, next) {
+   res.render('About'); 
 });
+
+/* GET Contact page */
+router.get('/Contact', function (req, res, next) {
+    res.render('Contact');
+});
+
+/* GET Service page */
+router.get('/services', function (req, res, next) {
+    res.render('services');
+});
+
+/* GET Projects page */
+router.get('/Projects', function (req, res, next) {
+    res.render('Projects');
+});
+
 
 module.exports = router;
